c82b48a51f6f2de1071f669d4790bf729e583efc
Vladimir Guzman <guzman.vla@gmail.com>
2020-06-11 19:12:44 -0500

    main modified get schema added

--- src/main.js
+++ src/main.js
@@ -20,3 +20,92 @@
 
       return config.build();
     }
+
+var fixedSchema = {
+  companies: [
+    {
+      name: 'url',
+      label: 'Url',
+      dataType: 'STRING',
+      semantics: {
+        conceptType: 'DIMENSION'
+      }
+    },
+    {
+      name: 'id',
+      label: 'Company ID',
+      dataType: 'NUMBER',
+      semantics: {
+        conceptType: 'DIMENSION'
+      }
+    },
+    {
+      name: 'name',
+      label: 'Company Name',
+      dataType: 'STRING',
+      semantics: {
+        conceptType: 'DIMENSION'
+      }
+    },
+    {
+      name: 'company_sector',
+      label: 'Company Sector',
+      dataType: 'STRING',
+      semantics: {
+        conceptType: 'DIMENSION'
+      }
+    },
+    {
+      name: 'number_of_employees',
+      label: 'Number of employees',
+      dataType: 'NUMBER',
+      semantics: {
+        conceptType: 'DIMENSION'
+      }
+    },
+    {
+      name: 'owner',
+      label: 'Email owner',
+      dataType: 'STRING',
+      semantics: {
+        conceptType: 'DIMENSION'
+      }
+    },
+    {
+      name: 'picture_url',
+      label: 'Picture url',
+      dataType: 'STRING',
+      semantics: {
+        conceptType: 'DIMENSION'
+      }
+    },
+    {
+      name: 'last_viewed',
+      label: 'Last viewed date',
+      dataType: 'STRING',
+      semantics: {
+        conceptType: 'DIMENSION'
+      }
+    },
+    {
+      name: 'last_interaction',
+      label: 'Last interaction date',
+      dataType: 'STRING',
+      semantics: {
+        conceptType: 'DIMENSION'
+      }
+    },
+    {
+      name: 'created',
+      label: 'Created date',
+      dataType: 'STRING',
+      semantics: {
+        conceptType: 'DIMENSION'
+      }
+    },
+  ]
+};
+
+function getSchema(request) {
+  return {schema: fixedSchema[request.configParams.resource]};
+}
